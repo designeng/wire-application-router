25071b65aaab9afd05cfdc2093dd9b396dc614da
designeng <denissavenok@gmail.com>
2016-02-25 16:22:07 +0300

    wire tests inited

--- test/redux/index.spec.js
+++ test/redux/index.spec.js
@@ -2,6 +2,9 @@ import chai, { expect } from 'chai';
 import spies from 'chai-spies';
 import when from 'when';
 
+import wire                 from 'essential-wire';
+import wireDebugPlugin      from 'essential-wire/source/debug';
+
 chai.use(spies);
 
 describe('root ......',  () => {
@@ -9,13 +12,24 @@ describe('root ......',  () => {
     let root = {};
 
     const before = (done) => {
-        done();
+        wire({
+            $plugins: [
+                wireDebugPlugin
+            ],
+            test: 123
+        })
+        .then((context) => {
+            root = context;
+            done();
+        })
+        .otherwise((error) => console.log("ERROR::::", error))
     }
 
     beforeEach(before);
 
     it('should.............',  (done) => {
-        expect(true).to.be.ok;
+        expect(root.test).to.be.ok;
+        expect(root.test).to.equal(123);
         done();
     });
 
